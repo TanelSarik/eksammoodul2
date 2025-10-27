3beac80b4464dee3efc4d161ad9abdda33cb9042
tanel sarik <tanel.sarik@tptlive.ee>
2025-10-27 11:26:29 +0200

    Add nunjucks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import vituum from 'vituum';
+import nunjucks from '@vituum/vite-plugin-nunjucks'
 
 export default {
     plugins: [
         vituum(),
+        nunjucks({
+            root: './src'
+        }),
     ],
 }
